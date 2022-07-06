f98b97c546fe30b52509a88e469e00b3662bf7d4
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-06 15:36:15 +0200

    add maincontent

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,25 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <HeaderPage></HeaderPage>
-    <Main></Main>
-    <Footer></Footer>
+    <HeaderPage/>
+    <Main>
+      <MainContent/>
+    </Main>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import HeaderPage from './components/HeaderPage.vue'
+import MainContent from './components/MainContent.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
     HeaderPage,
+    MainContent,
   }
 }
 </script>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <span>content go here</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainContent',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        background-color: black;
+        color: white;
+    }
+</style>
